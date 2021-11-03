bddab916c5d3ac1f2f276eab71a07a0eb9441d18
Leonerdo15 <leonardolage10@gmail.com>
2021-11-03 01:01:40 +0000

    Initial commit

---
+++ src/test/java/com/iade/ulide/UlideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iade.ulide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
