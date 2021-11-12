469ca8c42bc6d165bc3a3edc6ed1021662e678ed
Leonerdo15 <leonardolage10@gmail.com>
2021-11-12 15:54:54 +0000

    test new functions

---
+++ src/main/java/com/iade/ulide/models/repositories/ProgressRouteRepository.java
@@ -0,0 +1,7 @@
+package com.iade.ulide.models.repositories;
+
+import com.iade.ulide.models.ProgressRoute;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProgressRouteRepository extends CrudRepository<ProgressRoute, Integer> {
+}
--- src/main/java/com/iade/ulide/models/repositories/UserAchievementRepository.java
+++ src/main/java/com/iade/ulide/models/repositories/UserAchievementRepository.java
@@ -1,7 +1,21 @@
 package com.iade.ulide.models.repositories;
 
 import com.iade.ulide.models.UserAchievement;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserAchievementRepository extends CrudRepository<UserAchievement, Integer> {
+    String QueryFindUserAchievementsByUaUsId2 = "select count(*) from user_achievements";
+
+    Iterable<UserAchievement> findUserAchievementsByUaUsId(int id);
+
+    @Query(value = "select ac.ac_id, ua.ua_id, ac.ac_name, ua_ac_id, ua_date, ua_us_id from user_achievements ua, achievements ac where ua.ua_ac_id = ac.ac_id and ua.ua_us_id = ?1"  , nativeQuery = true)
+    Iterable<UserAchievement> findUserAchievementsByUaUsId2(int id);
+
+    long countByUaAcId(Integer uaAcId);
+
+
+
+
 }
