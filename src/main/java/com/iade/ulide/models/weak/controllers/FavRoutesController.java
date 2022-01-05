4766a9a542562b03a00cf68c4c624f8c7f2f4d25
Felipe Campelo Sabbado <fsabbado@gmail.com>
2022-01-05 18:34:57 +0000

    Create FavSpots and FavRoutes Controllers

---
+++ src/main/java/com/iade/ulide/models/weak/controllers/FavRoutesController.java
@@ -0,0 +1,57 @@
+package com.iade.ulide.models.weak.controllers;
+
+import com.iade.ulide.models.exceptions.NotFoundException;
+import com.iade.ulide.models.exceptions.Response;
+import com.iade.ulide.models.weak.FavRoutes;
+import com.iade.ulide.models.weak.FavSpots;
+import com.iade.ulide.models.weak.repositories.FavRoutesRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/favRoutes")
+public class FavRoutesController {
+    private final Logger logger = LoggerFactory.getLogger(FavRoutesController.class);
+
+    @Autowired
+    private FavRoutesRepository favRoutesRepository;
+
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<FavRoutes> getFavRoutes() {
+        logger.info("Sending all favorite routes");
+        return favRoutesRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public FavRoutes getFavRoutes(@PathVariable int id) {
+        logger.info("Sending favorite route with id " + id);
+        Optional<FavRoutes> _favRoute = favRoutesRepository.findById(id);
+        if (!_favRoute.isPresent()) throw
+                new NotFoundException("" + id, "Favorite Route", "id");
+        else
+            return _favRoute.get();
+    }
+
+    @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public FavRoutes saveFavRoute(@RequestBody FavRoutes favRoutes) {
+        FavRoutes savedFavRoute = favRoutesRepository.save(favRoutes);
+        logger.info("Saving favorite route with id " + savedFavRoute.getId());
+        return savedFavRoute;
+    }
+
+    @DeleteMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response deleteFavRoute(@PathVariable int id) {
+        logger.info("Deleting favorite route with id " + id);
+        Optional<FavRoutes> _favRoute = favRoutesRepository.findById(id);
+        if (!_favRoute.isPresent()) throw
+                new NotFoundException("" + id, "Favorite Route", "id");
+        else
+            favRoutesRepository.deleteById(id);
+        return new Response("Deleted favorite route with id " + id, null);
+    }
+}
---
+++ src/main/java/com/iade/ulide/models/weak/controllers/FavSpotsController.java
@@ -0,0 +1,56 @@
+package com.iade.ulide.models.weak.controllers;
+
+import com.iade.ulide.models.exceptions.NotFoundException;
+import com.iade.ulide.models.exceptions.Response;
+import com.iade.ulide.models.weak.FavSpots;
+import com.iade.ulide.models.weak.repositories.FavSpotsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/favSpots")
+public class FavSpotsController {
+    private final Logger logger = LoggerFactory.getLogger(FavSpotsController.class);
+
+    @Autowired
+    private FavSpotsRepository favSpotsRepository;
+
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<FavSpots> getFavSpots() {
+        logger.info("Sending all favorite spots");
+        return favSpotsRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public FavSpots getFavSpot(@PathVariable int id) {
+        logger.info("Sending favorite spot with id " + id);
+        Optional<FavSpots> _favSpot = favSpotsRepository.findById(id);
+        if (!_favSpot.isPresent()) throw
+                new NotFoundException("" + id, "Favorite Spot", "id");
+        else
+            return _favSpot.get();
+    }
+
+    @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public FavSpots saveFavSpot(@RequestBody FavSpots favSpots) {
+        FavSpots savedFavSpot = favSpotsRepository.save(favSpots);
+        logger.info("Saving favorite spot with id " + savedFavSpot.getId());
+        return savedFavSpot;
+    }
+
+    @DeleteMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response deleteFavSpot(@PathVariable int id) {
+        logger.info("Deleting favorite spot with id " + id);
+        Optional<FavSpots> _favSpot = favSpotsRepository.findById(id);
+        if (!_favSpot.isPresent()) throw
+                new NotFoundException("" + id, "Favorite Spot", "id");
+        else
+            favSpotsRepository.deleteById(id);
+        return new Response("Deleted favorite spot with id " + id, null);
+    }
+}
